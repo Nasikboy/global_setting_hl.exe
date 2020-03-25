3c0e19365082c751579064bbf4bb5cc2068ba66f
anybody <we-are-legion2013@yandex.ru>
2020-03-25 23:33:20 +0300

    converter cfg to txt and read file

--- window.py
+++ window.py
@@ -2,6 +2,7 @@ from tkinter import *
 import os
 from tkinter import filedialog
 from tkinter import messagebox as mb
+import wx
 
 class Winw(object):
 
@@ -9,7 +10,13 @@ class Winw(object):
  def window(self):
   self.root = Tk()
   self.root.title("Embodiment Half-life 1")
-  self.root.geometry("700x300")
+  self.root.geometry("750x300")
+
+  sq_fit_size = 300
+
+
+
+
 
 
   path = Button(self.root,text = 'Путь к файлу к игре',command = windows_start.path_open).grid(row=1,column=0)
@@ -28,19 +35,34 @@ class Winw(object):
   FPS100 = Entry(self.root,width = 5).grid(row=6, column=4)
   FPS_show = Checkbutton(self.root, text='Установка счётчика ФПС').grid(row=7, column=0)
   game_player = Checkbutton(self.root, text='Отдаление экрана от рук персонажа').grid(row=7, column=0)
-  Start_script = Button(self.root, text='Выполнение выбранных действий',width=55,height = 5,background = 'aqua').grid(row=8, column=0)
+  Start_script = Button(self.root, text='Выполнение выбранных действий',width=55,height = 5,background = 'aqua',command = windows_start.joint).grid(row=8, column=0)
   self.root.mainloop()
 
 
  def path_open(self):
-    path = filedialog.askdirectory()
-    path_hl = path + '/hl.exe'
+    self.path = filedialog.askdirectory()
+    path_hl = self.path + '/hl.exe'
     try:
         f = open(path_hl)
         f.close()
-        path_label = Label(self.root,text = path).grid(row=1,column=1)
+        path_label = Label(self.root,text = self.path).grid(row=1,column=1)
     except FileNotFoundError:
         mb.showerror("Внимание", "Вы указали неправильный путь к игре!")
+ def joint(self):
+     try:
+      self.pathcfg = str(self.path + "/valve/config.cfg")
+      papa = self.path + "/valve/"
+     except FileNotFoundError:
+      mb.showerror("У вас нету файла с конфигурацией (в будущем добавим восстановление конфигурации)")
+     os.rename(papa + 'config.cfg', papa + 'config.txt')
+     ready = open(papa + 'config.txt',"w")
+
+     for line in ready.readlines():
+         print(line)
+
+
+
+
 
 
 #--- окно запуска
