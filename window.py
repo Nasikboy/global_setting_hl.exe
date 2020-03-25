b9c5f32063c7c5f9960ff280f71463327cfa13eb
anybody <we-are-legion2013@yandex.ru>
2020-03-25 22:12:14 +0300

    finish create interface

--- window.py
+++ window.py
@@ -9,7 +9,7 @@ class Winw(object):
  def window(self):
   self.root = Tk()
   self.root.title("Embodiment Half-life 1")
-  self.root.geometry("400x300")
+  self.root.geometry("700x300")
 
 
   path = Button(self.root,text = 'Путь к файлу к игре',command = windows_start.path_open).grid(row=1,column=0)
@@ -20,7 +20,15 @@ class Winw(object):
   Jump_up.grid(row=3, column=0)
   Jump_down = Checkbutton(self.root,text = 'Прыжок на колёсико вниз')
   Jump_down.grid(row=4, column=0)
-
+  speed = Checkbutton(self.root, text='Ускорение Фримена').grid(row=5,column=0)
+  FPS = Checkbutton(self.root, text='Установка бинда на фпс = 20/100').grid(row=6, column=0)
+  lbl_fps20 = Label(self.root,text = 'Кнопка для 20fps = ').grid(row=6,column = 1)
+  lbl_fps100 = Label(self.root, text='Кнопка для 100fps = ').grid(row=6, column=3)
+  FPS20 = Entry(self.root,width = 5).grid(row=6, column=2)
+  FPS100 = Entry(self.root,width = 5).grid(row=6, column=4)
+  FPS_show = Checkbutton(self.root, text='Установка счётчика ФПС').grid(row=7, column=0)
+  game_player = Checkbutton(self.root, text='Отдаление экрана от рук персонажа').grid(row=7, column=0)
+  Start_script = Button(self.root, text='Выполнение выбранных действий',width=55,height = 5,background = 'aqua').grid(row=8, column=0)
   self.root.mainloop()
 
 
