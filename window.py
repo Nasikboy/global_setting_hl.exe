c1cde5bca4055da2d2fabb1a3335c5cc73456bc4
anybody <we-are-legion2013@yandex.ru>
2020-03-29 20:41:48 +0300

    Сделал настройку (бхопа)

--- window.py
+++ window.py
@@ -16,28 +16,27 @@ class Winw(object):
 
 
 
-
+  var = IntVar()
 
 
   path = Button(self.root,text = 'Путь к файлу к игре',command = windows_start.path_open).grid(row=1,column=0)
-  Bunny_Hop = Checkbutton(self.root,text ='Подключить Bunny Hop')
-  Bunny_Hop.grid(row=2,column=0)
+  self.Bunny_Hop = Checkbutton(self.root,text ='Подключить Bunny Hop', variable=var)
+  self.Bunny_Hop.grid(row=2,column=0)
 
-  Jump_up = Checkbutton(self.root,text = 'Прыжок на колёсико вверх')
-  Jump_up.grid(row=3, column=0)
-  Jump_down = Checkbutton(self.root,text = 'Прыжок на колёсико вниз')
-  Jump_down.grid(row=4, column=0)
-  speed = Checkbutton(self.root, text='Ускорение Фримена').grid(row=5,column=0)
-  FPS = Checkbutton(self.root, text='Установка бинда на фпс = 20/100').grid(row=6, column=0)
+  self.Jump_up = Checkbutton(self.root,text = 'Прыжок на колёсико вверх')
+  self.Jump_up.grid(row=3, column=0)
+  self.Jump_down = Checkbutton(self.root,text = 'Прыжок на колёсико вниз')
+  self.Jump_down.grid(row=4, column=0)
+  self.speed = Checkbutton(self.root, text='Ускорение Фримена').grid(row=5,column=0)
+  self.FPS = Checkbutton(self.root, text='Установка бинда на фпс = 20/100').grid(row=6, column=0)
   lbl_fps20 = Label(self.root,text = 'Кнопка для 20fps = ').grid(row=6,column = 1)
   lbl_fps100 = Label(self.root, text='Кнопка для 100fps = ').grid(row=6, column=3)
-  FPS20 = Entry(self.root,width = 5).grid(row=6, column=2)
-  FPS100 = Entry(self.root,width = 5).grid(row=6, column=4)
-  FPS_show = Checkbutton(self.root, text='Установка счётчика ФПС').grid(row=7, column=0)
-  game_player = Checkbutton(self.root, text='Отдаление экрана от рук персонажа').grid(row=7, column=0)
+  self.FPS20 = Entry(self.root,width = 5).grid(row=6, column=2)
+  self.FPS100 = Entry(self.root,width = 5).grid(row=6, column=4)
+  self.FPS_show = Checkbutton(self.root, text='Установка счётчика ФПС').grid(row=7, column=0)
+  self.game_player = Checkbutton(self.root, text='Отдаление экрана от рук персонажа').grid(row=7, column=0)
   Start_script = Button(self.root, text='Выполнение выбранных действий',width=55,height = 5,background = 'aqua',command = windows_start.joint).grid(row=8, column=0)
   self.root.mainloop()
-
 
  def path_open(self):
     self.path = filedialog.askdirectory()
@@ -55,10 +54,34 @@ class Winw(object):
      except FileNotFoundError:
       mb.showerror("У вас нету файла с конфигурацией (в будущем добавим восстановление конфигурации)")
      os.rename(papa + 'config.cfg', papa + 'config.txt')
-     ready = open(papa + 'config.txt',"w")
+     os.rename(papa + 'autoexec.cfg', papa + 'autoexec.txt')
+     ready_autoexec = open(papa+'autoexec.txt',"r")
+     ready_config = open(papa + 'config.txt',"r")
+     bh_txt = 'alias "dj" "+jump;wait;-jump;wait;special"'
 
-     for line in ready.readlines():
-         print(line)
+     if self.Bunny_Hop.select:
+
+             str_abort = ""
+             for line in ready_autoexec.readlines():
+                 if bh_txt in line:
+                     str_abort = "ok"
+                 if str_abort == "":
+                     with open(papa+'autoexec.txt', 'a', encoding='utf-8') as f:
+                         f.write('alias "dj" "+jump;wait;-jump;wait;special"' + '\n')
+                         f.write('alias "+dj" "alias _special dj; dj"'+ '\n')
+                         f.write('alias "-dj" "alias _special"'+ '\n')
+                         f.write('bind SPACE "+dj'+ '\n')
+                     with open(papa + 'config.txt', 'a', encoding='utf-8') as f:
+                         f.write('exec autoexec.cfg'+ '\n')
+                     ready_autoexec.close()
+                     ready_config.close()
+
+     os.rename(papa + 'config.txt', papa + 'config.cfg')
+     os.rename(papa + 'autoexec.txt', papa + 'autoexec.cfg')
+
+#     for line in ready.readlines():
+ #        print(line)
+
 
 
 
